bac0f1e8806a17351f7d8db8590ac8484d65497e
yolokuku <YANGQIXIANG2001@163.com>
2022-10-12 14:44:40 +0800

    feat: 转为自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,28 @@
+/// <reference types="vitest" />
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // 按需导入element
-// import AutoImport from "unplugin-auto-import/vite";
-// import Components from "unplugin-vue-components/vite";
-// import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-// AutoImport({
-//   resolvers: [ElementPlusResolver()],
-// }),
-// Components({
-//   resolvers: [ElementPlusResolver()],
-// }),
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 // https://vitejs.dev/config/
 export default defineConfig({
+  test:{
+    deps: {
+      inline: ['element-plus'],
+    },
+  },
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
